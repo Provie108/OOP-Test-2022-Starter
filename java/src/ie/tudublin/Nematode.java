652c20f987dabd9c6aa0ecb652e4a0a2ff306903
C20499732 Providence Obiozor <c20499732@mytudublin.ie>
2022-04-27 23:24:20 +0100

    Almost There

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,121 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+import processing.data.TableRow;
+
+public class Nematode 
+{
+    private String name;
+    private int length;
+    private int limbs;
+    private String gender;
+    private int eyes;
+
+    @Override
+    public String toString()
+    {
+        return "Nematode [Name=" + name + ", length=" + length + ", limbs=" + limbs + ", gender=" + gender + ", eyes=" + eyes + "]";
+    }
+
+    public Nematode(TableRow tr)
+    {
+        this(tr.getString("name"), tr.getInt("length"), tr.getInt("limbs"), tr.getString("gender"), tr.getInt("eyes"));
+    }
+    
+    public Nematode(String name, int length, int limbs, String gender, int eyes)
+    {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+
+    public void render(NematodeVisualiser pa)
+    {
+        float x = 400;
+        float y = PApplet.map(getLength(), 0, 8, 200, 600);
+        float firstY = 0;
+        float d= 800 / getLength();
+
+        for(int j = 0; j < getLength(); j++)
+        {
+            pa.stroke(255);
+            pa.noFill();
+            y = PApplet.map(j, 0, getLength(), d / 2, 800 - (d / 2));
+
+            pa.circle(400, y, d- (d/3));
+
+            if(limbs == 1)
+            {
+                pa.line(400 - ((d- (d/3))/2), y, (400 - ((d- (d/3))/2)) - 50, y);
+                pa.line(400 + ((d- (d/3))/2), y, (400 + ((d- (d/3))/2)) + 50, y);
+            }
+
+            if(eyes == 1)
+            {
+                if(j == 0)
+                {
+                    pa.line(400, y - ((d- (d/3))/2), 375, y - ((d- (d/3))/2) - 25);
+                    pa.line(400, y - ((d- (d/3))/2), 425, y - ((d- (d/3))/2) - 25);
+                    pa.circle(375, y - ((d- (d/3))/2) - 25, 25);
+                    pa.circle(425, y - ((d- (d/3))/2) - 25, 25);
+                }
+            }
+
+            if(getGender() == "m")
+            {
+                if(j == 0)
+                {
+                    pa.line(400, y - ((d- (d/3))/2), 400, y - ((d- (d/3))/2) - 25);
+                    pa.circle(400, y - ((d- (d/3))/2) - 25, 25);
+                }
+            }
+
+            
+        }
+
+
+    }
+
+}
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -9,33 +9,87 @@ import processing.data.TableRow;
 public class NematodeVisualiser extends PApplet
 {
 
-	public void keyPressed()
-	{		
-		if (keyCode == LEFT)
-		{
-		}		
-	}
+    ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+
+    int i = 6;
+
+    public void keyPressed()
+    {
+        if (keyCode == LEFT)
+        {
+            i--;
+
+            if(i == 0)
+            {
+                i = 12;
+            }
+        }
+
+        if(keyCode == RIGHT)
+        {
+            i++;
+
+            if(i == 14)
+            {
+                i = 1;
+            }
+        }
+    }
 
 
-	public void settings()
-	{
-		size(800, 800);
-	}
+    public void settings()
+    {
+        size(800, 800);
+    }
 
-	public void setup() 
-	{
-		colorMode(HSB);
-		background(0);
-		smooth();				
-	}
-	
-
-	public void loadNematodes()
-	{
-	}
+    public void setup() 
+    {
+        colorMode(HSB);
+        background(0);
+        smooth();
+        loadNematodes();
+        printNematodes();
+    }
 
 
-	public void draw()
-	{	
-	}
+    public void loadNematodes()
+    {
+        Table table = loadTable("nematodes.csv", "header");
+        for(TableRow r:table.rows())
+        {
+            Nematode n = new Nematode(r);
+            nematodes.add(n);
+
+        }
+    }
+
+    public void printNematodes()
+    {
+        for(Nematode n:nematodes)
+        {
+            System.out.println(n);
+        }
+    }
+
+    public void arrows()
+    {
+        stroke(255);
+
+        line(100, 400, 250, 400);
+        line(100, 400, 150, 425);
+        line(100, 400, 150, 375);
+
+        line(700, 400, 550, 400);
+        line(700, 400, 650, 425);
+        line(700, 400, 650, 375);
+    }
+
+
+    public void draw()
+    {
+        background(0);
+        arrows();
+
+        nematodes.get(i).render(this);
+    }
 }
